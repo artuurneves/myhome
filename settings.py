5931e740f22b6bf436308d00d3cd44b125cc7d90
Artur Neves <artur.neves@scania.com>
2020-05-24 13:09:28 -0300

    version 0.0.1

--- settings.py
+++ settings.py
@@ -4,7 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 os.environ['FLASK_ENV'] = 'development'
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'myhome.db')
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'myhome.db')
+SQLALCHEMY_DATABASE_URI = 'postgres:///myhome'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 SECRET_KEY = 'MySecret'
